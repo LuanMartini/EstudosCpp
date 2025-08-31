0f49c4cde05e06f85406cd584c76becfd37fb440
Luan Martini <luancarlosmartini@gmail.com>
2025-08-31 00:10:59 -0300

    att 3

---
+++ Exercicios/estruturasequencial/3.cpp
@@ -0,0 +1,26 @@
+// Faça um programa em C++ que leia dois números inteiros e exiba o produto entre eles.
+// declarar variavel -- pedir numeros -- realizar operação -- imprimir eles
+
+#include <iostream>
+
+int main(){
+    int n1, n2, produto;
+
+    std::cout << "Digite o primeiro numero: ";
+    std::cin >> n1;
+
+    std::cout << "Digite o segundo numero: ";
+    std::cin >> n2;
+
+    produto = n1 * n2;
+    std::cout << "A multiplicação dos produtos é: " << produto << "\n";
+    return 0;
+
+
+
+
+
+
+
+
+}

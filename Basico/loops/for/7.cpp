cb5ad0a3ca8e310a37337695bb543ca456926acc
Luan Martini <luancarlosmartini@gmail.com>
2025-09-06 20:27:31 -0300

    quadrados 1-20

---
+++ Basico/loops/for/7.cpp
@@ -0,0 +1,14 @@
+// 7.  **Quadrados dos números**\
+//     Imprima os quadrados dos números de 1 até 20 (ex.: 1², 2², 3²...).
+#include <iostream>
+int main(){
+int i = 1;
+for (i; i <= 20; i++)
+{
+    std::cout << i*i << '\n';
+}
+return 0;
+
+
+
+}

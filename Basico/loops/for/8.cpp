401f673adae32e55ba0f93f8238333a9c204cb9f
Luan Martini <luancarlosmartini@gmail.com>
2025-09-06 20:36:38 -0300

    multiplos por 3 de 1-30

---
+++ Basico/loops/for/8.cpp
@@ -0,0 +1,16 @@
+// 8.  **Múltiplos de 3**\
+//     Imprima todos os múltiplos de 3 de 1 até 30.
+
+#include <iostream>
+
+int main(){
+int i = 1;
+for (i; i <= 30; i++)
+{
+   int multiplo = i*3;
+   std::cout << multiplo << '\n';
+}
+
+    return 0;
+
+}

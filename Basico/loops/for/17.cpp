e719b166ae32da5f6efebeb687d67ee6d112d009
Luan Martini <luancarlosmartini@gmail.com>
2025-09-09 10:21:24 -0300

    de boaça

---
+++ Basico/loops/for/17.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+	for (int i =1; i <=10; i++){
+    int cubodon;
+    cubodon = i*i*i;
+    std::cout << "o cubo de " << i << " é " << cubodon << '\n';
+    }
+    return 0;
+}
